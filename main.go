331504e503b10dea55ee68d22ebde7ffe791f35c
chouettevan <pybots743@gmail.com>
2024-04-23 08:09:28 -0400

    Added fuzzing goroutine

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"time"
+	"io"
+	"sync"
+	"fmt"
 	"github.com/alexflint/go-arg"
 )
 	
@@ -10,6 +15,10 @@ type args struct {
 	Port int `arg:"-p" default:"80" help:"http(s) server port"`
 	Config string `arg:"-f,required" help:"path to config file"`
 	Wordlists []string `arg:"-w,separate"`
+}
+type task struct {
+	request string
+	params []string
 }
 func (args) Description() string {
 return `ufuzz,unlike most web fuzzers,uses a config file containing an http request in which it will substitute the placeholders S1,S2,S3.. with input from the wordlists.
@@ -26,4 +35,25 @@ Placeholders can be used anywhere within the config file.
 func main() {
 	var args args
 	arg.MustParse(&args)
+}
+func fuzzer(connection io.ReadWriteCloser,mu *sync.Mutex,ch *chan task) {
+	defer connection.Close()
+	scan := bufio.NewScanner(connection)	
+	var status uint16
+	var delay int64
+	for {
+		mu.Lock()
+		tsk := <- *ch
+		mu.Unlock()
+		delay = time.Now().Unix()
+		_,err := connection.Write([]byte(tsk.request))
+		delay = time.Now().Unix() - delay
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		scan.Scan() 
+		fmt.Sscanf(scan.Text(),"HTTP/1.1 %d",status)
+	}
+
 }
