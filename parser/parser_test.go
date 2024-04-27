59b28678ff6450a64c99514c02891c76a3b0e20f
chouettevan <pybots743@gmail.com>
2024-04-27 14:01:30 -0400

    Added benchmarks

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -12,12 +12,13 @@ type testMap[K any,T any] []struct{
 	output T
 }
 
+var statusTests  = testMap[string,int]{
+	{"404 Not found response","HTTP/1.1 404 Not Found",404},
+	{"200 Ok response","HTTP/2.0 200 Ok",200},
+	{"302 Found response test","HTTP/1.1 302 Found",302},
+};
+
 func TestGetStatus(t *testing.T) {
-	var statusTests  = testMap[string,int]{
-		{"404 Not found response","HTTP/1.1 404 Not Found",404},
-		{"200 Ok response","HTTP/2.0 200 Ok",200},
-		{"302 Found response test","HTTP/1.1 302 Found",302},
-	};
 	for _,test := range statusTests {
 		t.Run(test.name,func(t *testing.T) {
 			if getStatus(test.input) != test.output {
@@ -27,11 +28,21 @@ func TestGetStatus(t *testing.T) {
 	}
 }
 
-const charset string  = "\n ";
-func TestHttpParse(t *testing.T) {
-	var parseTests = testMap[io.Reader,string]{
-		{"200 Ok with keep-alive",strings.NewReader("HTTP/1.1 200 Ok\nConnection:keep-alive"),"200    37"},
+func BenchmarkGetStatus(b *testing.B) {
+	for _,test := range statusTests {
+		b.Run(test.name,func(b *testing.B) {
+			for i := 0;i < b.N;i++ {
+				getStatus(test.input)
+			}
+		})
 	}
+}
+
+const charset string  = "\n ";
+var parseTests = testMap[io.Reader,string]{
+	{"200 Ok with keep-alive",strings.NewReader("HTTP/1.1 200 Ok\nConnection:keep-alive"),"200    37"},
+}
+func TestHttpParse(t *testing.T) {
 	for _,test := range parseTests {
 		t.Run(test.name,func(t *testing.T) {
 			out := strings.Trim(HttpParse(test.input),charset)
@@ -41,3 +52,14 @@ func TestHttpParse(t *testing.T) {
 		})
 	}
 }
+func BenchmarkHttpParse(b *testing.B) {
+	for _,test := range parseTests {
+		b.Run(test.name,func(b *testing.B) {
+			for i := 0;i < b.N;i++ {
+				HttpParse(test.input)
+			}
+		})
+	}
+
+}
+
